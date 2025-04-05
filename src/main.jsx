098366b39fa0c6f57868ef9e4eee0f5c79fd1830
amontral <55558445+amontral@users.noreply.github.com>
2025-04-04 21:08:38 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,16 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
 import Assessment from './Assessment';
+import Services from './Services';
+import Contact from './Contact';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/assessment" element={<Assessment />} />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/assessment" element={<Assessment />} />
+      <Route path="/services" element={<Services />} />
+      <Route path="/contact" element={<Contact />} />
+    </Routes>
+  </BrowserRouter>
 );
