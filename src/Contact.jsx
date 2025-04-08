a2d89177a3ca21f76a8190aab694d3d83274f114
amontral <55558445+amontral@users.noreply.github.com>
2025-04-08 12:48:02 -0500

    Update Contact.jsx

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -5,6 +5,7 @@ export default function Contact() {
     <div style={{ padding: '2rem', color: 'white', backgroundColor: '#0c0c0e', minHeight: '100vh' }}>
       <h2>Contact Us</h2>
       <p>Email: contact@silexstrategicgroup.com</p>
+<p>Phone: 501-952-7172</p>
     </div>
   );
 }
